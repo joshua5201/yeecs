f5fe5024f6069bc9120b78bf79d57c7fa11d761f
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-05-24 20:48:37 +0800

    Tweak some div size

--- app/assets/javascripts/subpage.js
+++ app/assets/javascripts/subpage.js
@@ -3,12 +3,18 @@ $(document).ready(function(){
     var w = $("#box-side").width();
     $("#display-side").css("width",w);
 
+    var p_index_height = $(".p-index-img").height();
+    $(".p-index-word").css("height", p_index_height*0.7);
+
 });
 
 
 $(window).resize(function(){
     var w = $("#box-side").width();
     $("#display-side").css("width",w);
+
+    var p_index_height = $(".p-index-img").height();
+    $(".p-index-word").css("height", p_index_height*0.7);
 
 });
 
