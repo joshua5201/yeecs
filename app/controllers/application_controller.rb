96a9b3c27a0265390857ff1e7c927cb0ebbaaeb1
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-06-28 17:46:32 +0800

    Finish url-based i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@ class ApplicationController < ActionController::Base
   include HttpAcceptLanguage::AutoLocale
   protect_from_forgery with: :exception
   before_action :set_navbar_contents
+  before_action :override_locale
 
   private
+
+  def override_locale
+    default = I18n.locale
+    begin
+      session[:locale] = params[:locale] if params[:locale]
+      I18n.locale = session[:locale]
+    rescue I18n::InvalidLocale
+      session[:locale] = nil
+      I18n.locale = default
+    end
+  end
 
   def set_navbar_contents
     @categories = Category.includes(:pages).order(:id => :asc)
