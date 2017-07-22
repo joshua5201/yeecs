7d3a02e336f5f1bf642d6944618000e5dcecbb68
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-07-22 16:59:26 +0800

    Finish a lot of things

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,6 @@
 class MembersController < ApplicationController
   def show
+    @member = Member.find(params[:id])
+    @posts = Post.order(id: :desc).first(5)
   end
 end
--- app/controllers/post_categories_controller.rb
+++ app/controllers/post_categories_controller.rb
@@ -1,7 +1,7 @@
 class PostCategoriesController < ApplicationController
   def show
     @post_category = PostCategory.find(params[:id])
-    @posts = @post_category.posts
+    @posts = @post_category.posts.page(params[:page]).per(10)
     render('posts/index')
   end
 end
