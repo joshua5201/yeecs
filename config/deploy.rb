b6128dac3f04a7f4b6596abe19fe507ef34e87dd
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-07-28 21:36:35 +0800

    Fix typo in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, "git@github.com:joshua5201/yeecs.git"
 
 # Default value for :linked_files is []
 #append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, "db/developent.sqlite3", "db/staging.sqlite3", "db/production.sqlite3"
+append :linked_files, "db/development.sqlite3", "db/staging.sqlite3", "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
