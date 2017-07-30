a40259cbe11991a56e39573c0c427067b2160c43
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-07-30 17:36:37 +0800

    Fix typo QQ

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def t_attr(attr)
-    en_atrr = "en_#{attr.to_s}".to_sym
+    en_attr = "en_#{attr.to_s}".to_sym
     if I18n.locale != 'zh-TW'.to_sym and send(en_attr).present?
       return send(en_attr)
     else
