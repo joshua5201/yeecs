650ac0424a652f88f201e07a48f1401d6856fc5b
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-08-27 17:27:18 +0800

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,8 +40,8 @@ attributes[:page][:is_pure_link] += [false, false, false, false, false]
 attributes[:page][:category_id] += [2, 2, 2, 2, 2]
 
 ## Category #3 系所成員
-attributes[:page][:title] += %w{班務委員 輔導教官與諮商老師 行政人員}
-attributes[:page][:en_title] += %w{Faculty\ Commitee Consultant Staff}
+attributes[:page][:title] += %w{單位主管 班務委員 輔導教官 諮商老師 行政人員}
+attributes[:page][:en_title] += %w{Manager Faculty\ Commitee Millitary Instructor Consultant Staff}
 attributes[:page][:url] += %w{/member_categories/faculty /member_categories/consultant /member_categories/staff}
 attributes[:page][:is_pure_link] += [true, true, true]
 attributes[:page][:category_id] += [3, 3, 3]
@@ -82,11 +82,11 @@ attributes[:post_category][:url] = %w{announcement seminar international contest
 
 # MemberCategory
 attributes[:member_category] = Hash.new
-attributes[:member_category][:title] = %w{班務委員 輔導教官與諮商老師 行政人員}
-attributes[:member_category][:en_title] = %w{Faculty\ Commitee Consultant Staff}
+attributes[:member_category][:title] = %w{單位主管 班務委員 輔導教官 諮商老師 行政人員}
+attributes[:member_category][:en_title] = %w{Manager Faculty\ Commitee Millitary Instructor Consultant Staff}
 attributes[:member_category][:url] = %w{faculty consultant staff}
 
-# MemberCategory
+# MemberDepartment
 attributes[:member_department] = Hash.new
 attributes[:member_department][:title] = %w{電機系 電子系 光電系 資工系}
 attributes[:member_department][:en_title] = %w{ECE EE IEO CS}
@@ -200,10 +200,18 @@ AffiliatedSchool.create([
 ])
 
 
-# Member
+# Managers
+#
 Member.create([
-  {name: '洪崇智', member_category_id: 1, member_department: 1, title: '教授', full_title: '交通大學電機工程學系教授（兼電機資訊學士班主任）', education_background: '美國俄亥俄州立大學電機工程博士', 
+  {name: '洪崇智', member_category_id: 1, title: '教授', full_title: '交通大學電機工程學系教授（兼電機資訊學士班主任）', education_background: '美國俄亥俄州立大學電機工程博士', 
 content: ' 
+姓名：洪崇智<br>
+聯絡電話：03-5712121 #54535<br>
+Email：cchung@mail.nctu.edu.tw<br>
+Office Hour：Tuesday CD、Wednesday CD、Thursday CD<br>
+專長領域：類比與混合信號積體電路設計、電源管理積體電路設計、生醫積體電路設計<br>
+<a href="http://cmbsd.cm.nctu.edu.tw/~aiclab/">個人網址</a><br>
+<a href="http://cmbsd.cm.nctu.edu.tw/~aiclab/" >實驗室：類比積體電路實驗室</a><br>
 ',
 en_content: ' 
 Name：Chung-Chih Hung<br>
@@ -217,22 +225,75 @@ Research Field：Analog and Mixed-Signal IC Design, Power Management IC Design, 
 }])
 
 Member.create([
-  {name: '曹孝櫟', member_category_id: 1, member_department_id: 4, title: '副教授', full_title: '交通大學資訊工程學系副教授（兼電機資訊學士班副主任）', education_background: '國立成功大學工程科學博士', 
+  {name: '曹孝櫟', member_category_id: 1, title: '副教授', full_title: '交通大學資訊工程學系副教授（兼電機資訊學士班副主任）', education_background: '國立成功大學工程科學博士', 
 content: '
-姓名：曹孝櫟
-聯絡電話：03-5712121 #54717
-Email：sltsao@cs.nctu.edu.tw
-Office Hour：Monday CDGH、Wednesday CD
-專長領域：節能計算、嵌入式系統、無線網路
+姓名：曹孝櫟<br>
+聯絡電話：03-5712121 #54717<br>
+Email：sltsao@cs.nctu.edu.tw<br>
+Office Hour：Monday CDGH、Wednesday CD<br>
+專長領域：節能計算、嵌入式系統、無線網路<br>
 <a href="http://people.cs.nctu.edu.tw/~sltsao/">個人網址：</a><br>
 實驗室：綠色運算與嵌入式系統實驗室
 ',
 en_content: '
+姓名：曹孝櫟<br>
+聯絡電話：03-5712121 #54717<br>
+Email：sltsao@cs.nctu.edu.tw<br>
+Office Hour：Monday CDGH、Wednesday CD<br>
+專長領域：節能計算、嵌入式系統、無線網路<br>
+<a href="http://people.cs.nctu.edu.tw/~sltsao/">個人網址：</a><br>
+實驗室：綠色運算與嵌入式系統實驗室
+'}
+])
+
+# Member
+
+Member.create([
+  {name: '洪崇智', member_category_id: 2, member_department_id: 1, title: '教授', full_title: '交通大學電機工程學系教授（兼電機資訊學士班主任）', education_background: '美國俄亥俄州立大學電機工程博士', 
+content: ' 
+姓名：洪崇智<br>
+聯絡電話：03-5712121 #54535<br>
+Email：cchung@mail.nctu.edu.tw<br>
+Office Hour：Tuesday CD、Wednesday CD、Thursday CD<br>
+專長領域：類比與混合信號積體電路設計、電源管理積體電路設計、生醫積體電路設計<br>
+<a href="http://cmbsd.cm.nctu.edu.tw/~aiclab/">個人網址</a><br>
+<a href="http://cmbsd.cm.nctu.edu.tw/~aiclab/" >實驗室：類比積體電路實驗室</a><br>
+',
+en_content: ' 
+Name：Chung-Chih Hung<br>
+Contact：03-5712121 #54535<br>
+Email：cchung@mail.nctu.edu.tw<br>
+Office Hour：Tuesday CD、Wednesday CD、Thursday CD<br>
+Research Field：Analog and Mixed-Signal IC Design, Power Management IC Design, Biomedical IC Design<br>
+<a href="http://cmbsd.cm.nctu.edu.tw/~aiclab/">Homepage</a><br>
+<a href="http://cmbsd.cm.nctu.edu.tw/~aiclab/" >Lab. ：Analog & Mixed-Signal Integrated Circuit Laboratory</a><br>
+'
+}])
+
+Member.create([
+  {name: '曹孝櫟', member_category_id: 2, member_department_id: 4, title: '副教授', full_title: '交通大學資訊工程學系副教授（兼電機資訊學士班副主任）', education_background: '國立成功大學工程科學博士', 
+content: '
+姓名：曹孝櫟<br>
+聯絡電話：03-5712121 #54717<br>
+Email：sltsao@cs.nctu.edu.tw<br>
+Office Hour：Monday CDGH、Wednesday CD<br>
+專長領域：節能計算、嵌入式系統、無線網路<br>
+<a href="http://people.cs.nctu.edu.tw/~sltsao/">個人網址：</a><br>
+實驗室：綠色運算與嵌入式系統實驗室
+',
+en_content: '
+姓名：曹孝櫟<br>
+聯絡電話：03-5712121 #54717<br>
+Email：sltsao@cs.nctu.edu.tw<br>
+Office Hour：Monday CDGH、Wednesday CD<br>
+專長領域：節能計算、嵌入式系統、無線網路<br>
+<a href="http://people.cs.nctu.edu.tw/~sltsao/">個人網址：</a><br>
+實驗室：綠色運算與嵌入式系統實驗室
 '}
 ])
 
 Member.create([
-  {name: ' 蘇育德', member_category_id: 1, member_department_id: 1, title: '教授', full_title: '交通大學電機工程學系教授', education_background: '美國南加州大學電機博士', 
+  {name: ' 蘇育德', member_category_id: 2, member_department_id: 1, title: '教授', full_title: '交通大學電機工程學系教授', education_background: '美國南加州大學電機博士', 
 content: '
 姓名：蘇育德<br>
 聯絡電話：03-5731820<br>
@@ -241,12 +302,21 @@ Office Hour：Wednesday GH<br>
 專長領域：通訊系統設計、無線網路、統計信號處理、錯誤控制<br>
 <a href="http://www.eed.nctu.edu.tw/people/bio.php?PID=87">個人網址</a><br>
 實驗室：通訊與網路研究室
+  ',
+  en_content: '
+Name：Su Yu Ted<br>
+Contact：03-5731820<br>
+Email：ytsu@mail.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+Research Field：Communication System Design, Wireless Networks, Statistical Signal Processing, Error Control Theory<br>
+<a href="http://www.eed.nctu.edu.tw/people/bio.php?PID=87">homepage</a><br>
+Lab. ：Communication and Network Research Lab
   '}
 ])
 
 Member.create([
   {name: '張添烜', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 2,
   title: '副教授', 
   content: '
@@ -259,12 +329,19 @@ Office Hour：Wednesday GH<br>
 實驗室：VLSI Signal Processing Lab
   ',
   en_content: '
+Name：Tian-Sheuan Chang<br>
+Contact：03-5731925<br>
+Email：tschang@twins.ee.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+Research Field：System-on-Chip Design, Multimedia Signal Processing, Computer Architecture<br>
+<a href="https://sites.google.com/site/tiansheuanchang/">Homepage</a><br>
+Lab. ：VLSI Signal Processing Lab
 '}
 ])
 
 Member.create([
   {name: '陳富強', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 2, 
   title: '副教授', 
   full_title: '交通大學電機工程學系副教授', 
@@ -278,12 +355,19 @@ Office Hour：Wednesday GH<br>
 <a href="http://www.eed.nctu.edu.tw/people/bio.php?PID=93">個人網址</a>
 實驗室：應用電磁實驗室
   ', en_content: '
+Name：Chen, Fu-Chiarng<br>
+Contact：03-5131428<br>
+Email：fchen@faculty.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+Research Field：Applied Electromagnetics, Microwave Engineering, Antenna Design, Mobile Phone Communication System<br>
+<a href="http://www.eed.nctu.edu.tw/people/bio.php?PID=93">Homepage</a>
+Lab. ：Applied Electromagnetics Laboratory (http://www.eed.nctu.edu.tw/app-op/lab.php?Sn=222)
   '}
 ])
 
 Member.create([
   {name: '廖育德', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 1, 
   title: '助理教授', 
   full_title: ' 交通大學電機工程學系助理教授', 
@@ -297,26 +381,47 @@ Office Hour：Wednesday GH<br>
 <a href="http://www.eed.nctu.edu.tw/people/bio.php?PID=10456">個人網址</a><br>
 實驗室：Wireless Integrated Microsystems Lab
   ', en_content: '
+Name：Liao, Yu-Te<br>
+Contact：03-5712121 #54394<br>
+Email：yudoliao@g2.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+Research Field：Analog/RF Circuits and Systems, Biomedical Electronics, Wireless Sensor Systems and Interface Design<br>
+<a href="http://www.eed.nctu.edu.tw/people/bio.php?PID=10456">Homepage</a><br>
+Lab. ：Wireless Integrated Microsystems Lab
   '}
 ])
 
 Member.create([
   {name: '吳重雨', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 1, 
   title: '教授', 
   full_title: '交通大學電機工程學系教授', 
   education_background: '', 
   content: '
-
+姓名：吳重雨<br>
+聯絡電話：03-5712121 #54148<br>
+Email：peterwu@mail.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+聯絡電話：Biomedical electronic devices and systems, intelligent bio-inspired vision sensor systems, nanoeletronic circuits and systems for RF/microwave communication<br>
+<a href="http://www.ics.ee.nctu.edu.tw/~cywu/">個人網頁</a><br>
+<a href="http://www.alab.ee.nctu.edu.tw/wpmu/ed307/" >Lab. </a>
   ', en_content: '
+  Name：Chung-Yu Wu<br>
+Contact：03-5712121 #54148<br>
+Email：peterwu@mail.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+Research Field：Biomedical electronic devices and systems, intelligent bio-inspired vision sensor systems, nanoeletronic circuits and systems for RF/microwave communication<br>
+<a href="http://www.ics.ee.nctu.edu.tw/~cywu/">Homepage</a><br>
+<a href="http://www.alab.ee.nctu.edu.tw/wpmu/ed307/" >Lab. </a>
+
   '}
 ])
 
 
 Member.create([
   {name: '賴伯承', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 2, 
   title: '助理教授', 
   full_title: '交通大學電子工程學系助理教授', 
@@ -330,12 +435,19 @@ Office Hour：Thursday EF <br>
 <a href="https://sites.google.com/a/g2.nctu.edu.tw/bclai">個人網址</a> <br>
 實驗室：平行運算系統實驗室 <br>
   ', en_content: '
+Name：Bo-Cheng Lai <br>
+Contact：03-5731927 <br>
+Email：bclai@mail.nctu.edu.tw <br>
+Office Hour：Thursday EF <br>
+Research Field：SoC and VLSI System Design, Computer-Aided Design, Multi-Core System Design <br>
+<a href="https://sites.google.com/a/g2.nctu.edu.tw/bclai">Homepage</a> <br>
+Lab. ：Parallel Computing System Lab (https://sites.google.com/a/g2.nctu.edu.tw/parallel-computing-lab/)<br>
   '}
 ])
 
 Member.create([
   {name: '林怡欣', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 3, 
   title: '副教授', 
   full_title: '交通大學光電工程學系副教授', 
@@ -349,13 +461,20 @@ Office Hour：Wednesday GH <br>
 <a href="http://web.it.nctu.edu.tw/~yilin/">個人網址</a> <br>
 實驗室：液態晶體實驗室
   ', en_content: '
+Name：Yi-Hsin Lin <br>
+Contact：03-5712121 #56376 <br>
+Email：yilin@mail.nctu.edu.tw <br>
+Office Hour：Wednesday GH <br>
+Research Field：Tunable liquid crystal devices, physics and optics of liquid crystal, surface and condense physics<br>
+<a href="http://web.it.nctu.edu.tw/~yilin/">Homepage</a> <br>
+Lab. ：Liquid Crystal Laboratory (http://web.it.nctu.edu.tw/~yilin/)
   '}
 ])
 
 
 Member.create([
   {name: '單智君', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 4, 
   title: '副教授', 
   full_title: '交通大學資訊工程學系副教授', 
@@ -369,12 +488,19 @@ Office Hour：Wednesday GH <br>
 <a href="https://www.cs.nctu.edu.tw/cswebsite/members/detail/jjshann">個人網址</a> <br>
 實驗室：系統實驗室
   ', en_content: '
+Name：Jyh-Jiun Shann <br>
+Contact：03-5731832 <br>
+Email：jjshann@cs.nctu.edu.tw <br>
+Office Hour：Wednesday GH <br>
+Research Field：Computer Architecture, Parallel Processing, Embedded Systems, Just-in-time Compilation <br>
+<a href="https://www.cs.nctu.edu.tw/cswebsite/members/detail/jjshann">個人網址</a> <br>
+Lab.：
   '}
 ])
 
 Member.create([
   {name: '李毅郎', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 4, 
   title: '教授', 
   full_title: '交通大學資訊工程學系教授', 
@@ -388,12 +514,19 @@ Office Hour：Wednesday GH <br>
 <a href="http://people.cs.nctu.edu.tw/~ylli/">個人網址</a> <br>
 超大型積體電路設計自動化實驗室
   ', en_content: '
+Name：Yih-Lang Li <br>
+Contact：03-5131364 <br>
+Email：ylli@cs.nctu.edu.tw <br>
+Office Hour：Wednesday GH <br>
+Research Field：Design Automation for Nanometer IC, Combinatorial Optimization, Many-Core Hardware Accelerator, Parallel Processing <br>
+<a href="http://people.cs.nctu.edu.tw/~ylli/">個人網址</a> <br>
+Lab. ：VLSI Desing Automation Lab
   '}
 ])
 
 Member.create([
   {name: '游逸平', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 4, 
   title: '助理教授', 
   full_title: '交通大學資訊工程學系助理教授', 
@@ -407,12 +540,19 @@ Office Hour：Monday CD <br>
 <a href="http://people.cs.nctu.edu.tw/~ypyou/">個人網址</a> <br>
 實驗室：系統軟體實驗室
   ', en_content: '
+Name：Yi-Ping You <br>
+Contact：03-5712121 #56688 <br>
+Email：ypyou@cs.nctu.edu.tw <br>
+Office Hour：Monday CD <br>
+Research Field：Optimizing compilers, system software, GPU virtualization <br>
+<a href="http://people.cs.nctu.edu.tw/~ypyou/">Homepage</a> <br>
+Lab. ：System Software Lab (http://sslab.cs.nctu.edu.tw/)
   '}
 ])
 
 Member.create([
   {name: '嚴力行', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 4, 
   title: '教授', 
   full_title: '交通大學資訊工程學系教授', 
@@ -424,12 +564,17 @@ Email：lhyen@cs.nctu.edu.tw</br>
 專長領域：無線網路、分散式計算、賽局理論</br>
 <a href="http://people.cs.nctu.edu.tw/~lhyen/">個人網址</a>
   ', en_content: '
+Name：Li-Hsing Yen</br>
+Contact：03-5712121 #54755</br>
+Email：lhyen@cs.nctu.edu.tw</br>
+Research Field：Wireless Networking, Distributed Computing, Game Theory</br>
+<a href="http://people.cs.nctu.edu.tw/~lhyen/">homepage</a>
   '}
 ])
 
 Member.create([
   {name: '董蘭榮', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 1, 
   title: '助理教授', 
   full_title: '交通大學資訊工程學系助理教授', 
@@ -443,12 +588,19 @@ Office Hour：Monday EF</br>
 <a href="http://soclab.cn.nctu.edu.tw/">個人網址</a></br>
 實驗室：系統晶片實驗室/ VLSI實驗室
   ', en_content: '
+Name：Dung, Lan-Rong</br>
+Contact：03-5131567</br>
+Email：lennon@faculty.nctu.edu.tw</br>
+Office Hour：Monday EF</br>
+Research Field：DSP IC Design, Battery Management System, Biomedical Signal Processing</br>
+<a href="http://soclab.cn.nctu.edu.tw/">Homepage</a></br>
+Lab.：SOC Lab(http://soclab.cn.nctu.edu.tw/)
   '}
 ])
 
 Member.create([
   {name: '田仲豪', 
-  member_category_id: 1, 
+  member_category_id: 2, 
   member_department_id: 3, 
   title: '教授', 
   full_title: '交通大學光電工程學系教授', 
@@ -462,5 +614,54 @@ Office Hour：Wednesday GH<br>
 <a href="http://www.ieo.nctu.edu.tw/oislab/">個人網址</a><br>
 實驗室：光資訊系統實驗室
   ', en_content: '
+Name： Chung-Hao Tien<br>
+Contact：03-5712121 #31584<br>
+Email：chtien@mail.nctu.edu.tw<br>
+Office Hour：Wednesday GH<br>
+Research Field：Optical Design Micro-Optics Devices and Optical Data storage<br>
+<a href="http://www.ieo.nctu.edu.tw/oislab/">Homepage</a><br>
+Lab. ：Optical Information System Lab (http://www.ieo.nctu.edu.tw/oislab/)
   '}
 ])
+
+Member.create([
+  {name: '陳明仁', 
+  member_category_id: 3, 
+  title: '教官', 
+  full_title: '教官', 
+  education_background: '', 
+  content: '
+  Name：陳明仁
+Contact：03-5712121轉分機50707
+Email：chenmj50707@g2.nctu.edu.tw
+
+  ', en_content: '
+  Name：Li-Yen Wu
+Contact：03-5712121轉分機50707
+Email：chenmj50707@g2.nctu.edu.tw
+  '}
+])
+
+Member.create([
+  {name: '吳立研', 
+  member_category_id: 3, 
+  title: '諮商老師', 
+  full_title: '諮商老師', 
+  education_background: '', 
+  content: '
+姓名：Li-Yen Wu <br>
+聯絡電話：03-5712121#51319 <br>
+Email：liyenwu@nctu.edu.tw <br>
+辦公室： 學生活動中心三樓<br>
+研究領域：精神分析取向心理治療、多元文化議題、英語諮商  
+
+  ', en_content: '
+Name：Li-Yen Wu <br>
+Contact：03-5712121#51319 <br>
+Email：liyenwu@nctu.edu.tw <br>
+Office：Student Activity Center 3F <br>
+Field：Group and individual counseling, psychotherapy, and psychological testing
+  '}
+])
+
+
