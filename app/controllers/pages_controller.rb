7e4882e701357da474d7cc4b210a0ad42c470c15
Tsung-en Hsiao <joshua841025@gmail.com>
2020-03-20 16:13:54 +0900

    Redirect to category main_url when no en content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ArticlesController
     @banners = Banner.includes(:post).order(rank: :asc)
     @link_blocks = LinkBlock.order(id: :asc)
   end
+
+  def show
+    super
+    if I18.locale != 'zh-TW'.to_sym && @article.en_hidden && @article.category.main_url.present?
+      return redirect_to @article.category.main_url
+    end
+  end
 end
